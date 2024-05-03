398f3cc1b2085d7e7dc7508f7b2ccbd810f3084f
Ivan Zdravkov <ivanzdravcov@gmail.com>
2024-05-03 23:46:12 +0300

    exercise 5 full

--- Hangman-game_exercise_5.py
+++ Hangman-game_exercise_5.py
@@ -9,6 +9,7 @@
 # файл можно найти в той же папке где и сам скрипт
 
 import random
+import string
 
 
 def random_word():
@@ -27,42 +28,46 @@ def display_word(word: str, guessed_letters: list[str]):
         if letter in guessed_letters:
             display += letter
         else:
-            display += display + "_"
+            display += "_"
     return display
 
 
 def main():
-
+    print("Welcome")
+    alphabet = list(string.ascii_lowercase)  # Создание списка букв алфавита
     word = random_word()
     print(word)
     guessed_letters: list[str] = []
 
-    Life = 5
+    life = 5
 
     while True:
-        print("\nWord:", display_word(word, guessed_letters))
-        print("Attempts left:", Life)
+        print(f"{" ".join(alphabet).upper()}")
+        print("Word:", display_word(word, guessed_letters))
+        print("Life left:", life)
         guess = input("Guess a letter: ").lower()
-
-        if guess in guessed_letters:
+        print("")
+        if guess not in string.ascii_lowercase:
+            print("Guess pleace only one letter")
+        elif guess in guessed_letters:
             print("You've already guessed that letter.")
         elif guess in word:
+            alphabet.remove(guess)
             guessed_letters.append(guess)
             if display_word(word, guessed_letters) == word:
-                print("\nCongratulations! You guessed the word:", word)
+                print("Congratulations! You guessed the word:", word)
                 break
         else:
             guessed_letters.append(guess)
-            Life -= 1
+            alphabet.remove(guess)
+            life -= 1
             print("Incorrect guess.")
 
-        if Life == 0:
-            print("\nYou ran out of attempts. The word was:", word)
+        if life == 0:
+            print("You ran out of attempts. The word was:", word)
             break
 
     print("Thanks for playing!")
-
-    print("Welcome")
 
 
 if __name__ == "__main__":
