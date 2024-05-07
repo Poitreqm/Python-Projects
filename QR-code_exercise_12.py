11603c6d31dc8fd32eb962a574d8b0e85c402180
Ivan Zdravkov <ivanzdravcov@gmail.com>
2024-05-07 23:08:14 +0300

    exercise 12 upd

--- QR-code_exercise_12.py
+++ QR-code_exercise_12.py
@@ -17,18 +17,19 @@ qr_input = input("Write the text or URL you want to attach to QR-code: ")
 qr_name = input("Write the name of the image that will be saved on your computer: ")
 
 qrcode = segno.make(f"{qr_input}", micro=False)
-print(qrcode.designator)
-qrcode.save(f"{qr_name}.png", scale=15)
+qrcode.save(f"{qr_name}.png")
 
 
 image = Image.open(f"{qr_name}.png")
-image = image.resize((100, 100))
+image = image.resize((29, 29))
 rgb_im = image.convert("RGB")
 
-
+print()
 for y in range(image.size[1]):
     for x in range(image.size[0]):
         print(background(text=" ", rgb=rgb_im.getpixel((x, y))), end="")
+    print()  # когда прочитали всю строку - переносимся на новую - отпечатывает в консоле маленькую размер image.resize((29, 29))
+# print()  # когда прочитали всю строку - переносимся на новую - отпечатывает две большие под размер image.resize((100, 100))
 
 print("Deal!")
 print(
