4cb0a643e15283a4719183c8f0d146cbde9b738c
Ivan Zdravkov <ivanzdravcov@gmail.com>
2024-05-17 21:02:43 +0300

    exercise 33m 34 upd 2

--- Find-Words-Containing-Character(2942)_exercise_33.py
+++ Find-Words-Containing-Character(2942)_exercise_33.py
@@ -2,7 +2,7 @@
 # 2942. Find Words Containing Character
 # You are given a 0-indexed array of strings words and a character x.
 # Return an array of indices representing the words that contain the character x.
-# Note that the returned array may be in any order.
+# Note that the returned array may be in any order
 #
 
 words = ["leet", "code", "cod"]
