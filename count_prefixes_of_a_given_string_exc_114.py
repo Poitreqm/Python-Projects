bfceaacf850fab82832b2be180069bc80bc422a3
Ivan Zdravkov <ivanzdravcov@gmail.com>
2024-05-27 17:45:14 +0300

    exc 114

---
+++ count_prefixes_of_a_given_string_exc_114.py
@@ -0,0 +1,15 @@
+# 2255. Count Prefixes of a Given String
+# You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+# Return the number of strings in words that are a prefix of s.
+# A prefix of a string is a substring that occurs at the beginning of the string. A substring is a contiguous sequence of characters within a string.
+
+
+Words = ["a", "a"]
+s = "aa"
+
+count = 0
+for i in Words:
+    if s.startswith(i):
+        count += 1
+
+print(count)
